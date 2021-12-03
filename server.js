5599b68f2b74d1aec1908cda189f0052e4e8b352
kianw97 <kianw@live.unc.edu>
2021-12-03 15:46:57 -0500

    another endpoint and npm fix

--- server.js
+++ server.js
@@ -35,14 +35,14 @@ app.post("/app/new", (req, res) => {
 
 // READ a list of all users (HTTP method GET) at endpoint /app/users/
 app.get("/app/users", (req, res) => {	
-	const stmt = db.prepare("SELECT * FROM userinfo").all();
-	res.status(200).json(stmt);
+	const mes = db.prepare("SELECT * FROM userinfo").all();
+	res.status(200).json(mes);
 });
 
 // READ a single user (HTTP method GET) at endpoint /app/user/:id
 app.get("/app/user/:id", (req, res) => {	
-	const stmt = db.prepare("SELECT * FROM userinfo WHERE id = ?");
-	const result = stmt.get(req.params.id);
+	const mes = db.prepare("SELECT * FROM userinfo WHERE id = ?");
+	const result = mes.get(req.params.id);
 	if(result === undefined) {
 		res.status(404).json({"message":"User does not exists. (404)"})
 	} else {
@@ -50,7 +50,11 @@ app.get("/app/user/:id", (req, res) => {
 	}
 });
 // UPDATE a single user (HTTP method PATCH) at endpoint /app/update/user/:id
-
+app.patch("/app/update/user/:id", (req, res) => {	
+	const msg = db.prepare("UPDATE userinfo SET user = COALESCE(?,user), pass = COALESCE(?,pass) WHERE id = ?");
+	const info = msg.run(req.body.user, md5(req.body.pass), req.params.id);
+	res.status(200).json({"message" : info.changes+ " record updated: ID " +req.params.id + " (200)"});
+});
 // DELETE a single user (HTTP method DELETE) at endpoint /app/delete/user/:id
 
 // Default response for any other request
